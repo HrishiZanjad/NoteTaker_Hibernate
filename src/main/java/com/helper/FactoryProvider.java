71974794e3ca56d9d307b7ca7e8379556a805780
HrishiZanjad <hrishikeshzanjad98@gmail.com>
2022-10-09 22:36:27 +0530

    Creating Note Taker 1st Commit

---
+++ src/main/java/com/helper/FactoryProvider.java
@@ -0,0 +1,20 @@
+package com.helper;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryProvider {
+	public static SessionFactory factory;
+	public static SessionFactory getFactroy() {
+		if(factory==null) {
+			factory=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		}
+		return factory;
+	}
+	
+	public void closeFactroy() {
+		if(factory.isOpen()) {
+			factory.close();
+		}
+	}
+}
